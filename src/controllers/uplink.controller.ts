264c28fb28773a8183e568b5a3f09b8bf35cab64
Piyavut Chompuchit <>
2022-02-06 18:18:23 +0700

    fix deploy

--- src/controllers/uplink.controller.ts
+++ src/controllers/uplink.controller.ts
@@ -1,14 +1,14 @@
 import { IUplink } from '../interfaces/entity/uplink.interface';
-import uplinkService from '../services/uplink.service';
-import callBackService from '../services/callback.service';
+import UplinkService from '../services/uplink.service';
+import CallBackService from '../services/callback.service';
 import { NextFunction, Request, Response } from 'express';
 import { ICallBack } from '../interfaces/entity/callback.interface';
 import { UplinkDto } from '../dtos/payload/uplink/uplink.dto';
 import { CallBack } from '../models/callback.model';
 
 class UplinkController {
-  public service = new uplinkService();
-  public callBackService = new callBackService();
+  public service = new UplinkService();
+  public callBackService = new CallBackService();
 
   public findAll = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
@@ -23,7 +23,6 @@ class UplinkController {
   public callBack = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const uplinkData: UplinkDto = req.body;
-      console.log(uplinkData);
       const data: ICallBack = {
         device_type_id: uplinkData.deviceTypeId,
         device_id: uplinkData.deviceId,
@@ -31,7 +30,6 @@ class UplinkController {
         callback_status: 'success',
         created_date: new Date(),
       };
-      console.log(data);
       const result: CallBack = await this.callBackService.createCallBack(data);
 
       res.status(200).json({ data: result, message: 'create success' });
--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,14 @@ import './index';
 import App from './app';
 import AuthRoute from './routes/auth.route';
 import IndexRoute from './routes/index.route';
-import UsersRoute from './routes/users.route';
-import SigFoxRoute from './routes/sigfox.route';
+// import UsersRoute from './routes/users.route';
+import SigFoxRoute from './/routes/sigfox.route';
 import validateEnv from './utils/validateEnv';
 import UplinkRoute from './routes/uplink.route';
 import DeviceRoute from './routes/device.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new SigFoxRoute(), new UplinkRoute(), new DeviceRoute()]);
+const app = new App([new IndexRoute(), new AuthRoute(), new SigFoxRoute(), new UplinkRoute(), new DeviceRoute()]);
 
 app.listen();
